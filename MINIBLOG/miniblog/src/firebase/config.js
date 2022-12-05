22d89ef2cd3267825392f2d94719dfcb7f8920df
Leonardo Gama <leogama09@gmail.com>
2022-12-05 11:55:27 -0300

    Update config.js

--- MINIBLOG/miniblog/src/firebase/config.js
+++ MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCVe9I89un0xRU9ljAC1DCANG-uwOfHen0",
