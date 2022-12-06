0f9b29877a54de6d4ed31ec792c859d12b3de240
Leonardo Gama <leogama09@gmail.com>
2022-12-06 11:22:27 -0300

    Update About.js

--- MINIBLOG/miniblog/src/pages/About/About.js
+++ MINIBLOG/miniblog/src/pages/About/About.js
@@ -1,10 +1,16 @@
 //CSS
 import styles from "./About.module.css"
 
+import { Link } from "react-router-dom"
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>Sobre o Mini <span style={{ fontWeight: 'bold' }}>Blog</span></h2>
+        <p>Este projeto consiste em um blog feito com React no front-end e Firebase no backend.</p>
+        <Link to ="/posts/create" className="btn">
+          Criar post
+        </Link>
     </div>
   )
 }
