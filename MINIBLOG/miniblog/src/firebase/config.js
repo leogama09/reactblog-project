1f3936156440197ee54a8071309374d32cbbaf41
Leonardo Gama <leogama09@gmail.com>
2022-12-06 17:24:48 -0300

    Update config.js

--- MINIBLOG/miniblog/src/firebase/config.js
+++ MINIBLOG/miniblog/src/firebase/config.js
@@ -1,6 +1,10 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCVe9I89un0xRU9ljAC1DCANG-uwOfHen0",
   authDomain: "miniblog-5a546.firebaseapp.com",
@@ -10,6 +14,7 @@ const firebaseConfig = {
   appId: "1:303219296110:web:fb780a2912ba1f3348a37f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app)
