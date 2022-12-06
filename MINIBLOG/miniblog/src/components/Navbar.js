88f7fcfbcb4a46eaf8e1f6f991047ecde36b84a0
Leonardo Gama <leogama09@gmail.com>
2022-12-06 11:04:20 -0300

    Update Navbar.js

--- MINIBLOG/miniblog/src/components/Navbar.js
+++ MINIBLOG/miniblog/src/components/Navbar.js
@@ -1,8 +1,14 @@
 import { NavLink } from "react-router-dom"
 
+import { useAuthentication } from "../hooks/useAuthentication"
+
+import { useAuthValue } from "../context/AuthContext"
+
 import styles from "./Navbar.module.css"
 
 const Navbar = () => {
+    const { user } = useAuthValue()
+
   return (
     <nav className={styles.navbar}>
         <NavLink to="/" className={styles.brand}>
@@ -17,7 +23,9 @@ const Navbar = () => {
                     Home
                 </NavLink>
             </li>
-            <li>
+            {!user && (
+                <>
+                <li>
                 <NavLink 
                 to="/login" 
                 className={({isActive}) => (isActive ? styles.active : '')}
@@ -33,6 +41,28 @@ const Navbar = () => {
                     Cadastrar
                 </NavLink>
             </li>
+                </>
+            )}
+            {user && (
+                <>
+                <li>
+                <NavLink 
+                to="/posts/create" 
+                className={({isActive}) => (isActive ? styles.active : '')}
+                >
+                    Novo post
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to="/dashboard" 
+                className={({isActive}) => (isActive ? styles.active : '')}
+                >
+                    Dashboard
+                </NavLink>
+            </li>
+                </>
+            )}
             <li>
                 <NavLink 
                 to="/about" 
