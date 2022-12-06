19059f089282a15fe2456da46156527ba6c2b7db
Leonardo Gama <leogama09@gmail.com>
2022-12-06 11:04:18 -0300

    Update App.js

--- MINIBLOG/miniblog/src/App.js
+++ MINIBLOG/miniblog/src/App.js
@@ -17,6 +17,8 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   const [user, setUser] = useState(undefined)
@@ -47,6 +49,8 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/posts/create" element={<CreatePost />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
          </div>
         <Footer />
