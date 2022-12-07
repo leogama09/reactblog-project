891b7258291346408a3e97b7f75e78782e8f500a
Leonardo Gama <leogama09@gmail.com>
2022-12-07 18:45:37 -0300

    Update Dashboard.js

--- MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
         <h2>Dashboard</h2>
         <p>Gerencie os seus posts</p>
         {posts && posts.length === 0 ? (
@@ -31,13 +31,13 @@ const Dashboard = () => {
           </div>
         ) : (
           <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
 
           {posts && 
-            posts.map((post) => <div key={post.id}>
+            posts.map((post) => <div key={post.id} className={styles.post_row}>
             <p>{post.title}</p>
             <div>
                 <Link to={`/posts/${post.id}`} className="btn btn-outline">
