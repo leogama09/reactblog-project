4cb12c3aeb68d320170c34ea29b48dbc8029282f
Leonardo Gama <leogama09@gmail.com>
2022-12-07 15:28:49 -0300

    Update useFetchDocuments.js

--- MINIBLOG/miniblog/src/hooks/useFetchDocuments.js
+++ MINIBLOG/miniblog/src/hooks/useFetchDocuments.js
@@ -32,7 +32,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         if (search) {
           q = await query(
             collectionRef,
-            where("tags", "array-contains", search),
+            where("tagsArray", "array-contains", search),
             orderBy("createdAt", "desc")
           );
         } else if (uid) {
