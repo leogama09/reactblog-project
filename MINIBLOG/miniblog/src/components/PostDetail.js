433ec10070b694cbc5c785e35c1678fb8c909f31
Leonardo Gama <leogama09@gmail.com>
2022-12-07 14:31:10 -0300

    Update PostDetail.js

--- MINIBLOG/miniblog/src/components/PostDetail.js
+++ MINIBLOG/miniblog/src/components/PostDetail.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 
 const PostDetail = ({post}) => {
   return (
-    <div>
+    <div className={styles.post_detail}>
         <img src={post.image} alt={post.title} />
         <h2>{post.title}</h2>
-        <p>{post.createdBy}</p>
-        <div>
+        <p cllassName={styles.createdBy}>{post.createdBy}</p>
+        <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
                 <p key={tag}>
                     <span>#</span>
