2b69cc994dddf1b6c5ad5868733a76e4712f4ad3
Leonardo Gama <leogama09@gmail.com>
2022-12-07 15:28:47 -0300

    Update PostDetail.js

--- MINIBLOG/miniblog/src/components/PostDetail.js
+++ MINIBLOG/miniblog/src/components/PostDetail.js
@@ -7,7 +7,7 @@ const PostDetail = ({post}) => {
     <div className={styles.post_detail}>
         <img src={post.image} alt={post.title} />
         <h2>{post.title}</h2>
-        <p cllassName={styles.createdBy}>{post.createdBy}</p>
+        <p className={styles.createdBy}>{post.createdBy}</p>
         <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
                 <p key={tag}>
