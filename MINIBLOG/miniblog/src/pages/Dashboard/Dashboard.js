affaeea433a99313d170d6b914cce4ffecb8741b
Leonardo Gama <leogama09@gmail.com>
2022-12-07 16:52:07 -0300

    Update Dashboard.js

--- MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -10,8 +10,7 @@ const Dashboard = () => {
   const {user} =  useAuthValue()
   const uid = user.uid
 
-  // posts do usuario
-  const posts = []
+  const {documents: posts, loading} = useFetchDocuments("posts", null, uid)
 
   return (
     <div>
@@ -29,6 +28,8 @@ const Dashboard = () => {
             Tem posts!
           </div>
         )}
+
+        {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   )
 }
