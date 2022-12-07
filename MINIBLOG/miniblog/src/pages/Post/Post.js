2d1b6e2ce17ff771572186c690b94d234bf36c8d
Leonardo Gama <leogama09@gmail.com>
2022-12-07 15:56:54 -0300

    Update Post.js

--- MINIBLOG/miniblog/src/pages/Post/Post.js
+++ MINIBLOG/miniblog/src/pages/Post/Post.js
@@ -2,13 +2,19 @@ import styles from './Post.module.css'
 
 // hooks
 import {useParams} from 'react-router-dom'
+import { useFetchDocument } from '../../hooks/useFetchDocument'
 
 const Post = () => {
     const {id} = useParams()
+    const {document: post} = useFetchDocument("posts", id)
 
   return (
     <div>
-        <h1>Post {id}</h1>
+        {post && (
+            <>
+            <h1>{post.title}</h1>
+            </>
+        )}
     </div>
   )
 }
